131ae0a9c3792efeedbcb769e2636cf5f3c98037
utkarsh <us126132@gmail.com>
2024-07-11 15:59:34 +0530

    Initial Commit

---
+++ Areas/Identity/Data/gmailUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace gmail.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the gmailUser class
+public class gmailUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName ="nvarchar(100")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100")]
+    public string LastName { get; set; }
+
+
+}
+
